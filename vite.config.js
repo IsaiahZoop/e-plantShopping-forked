642c86024e02ab6d10ab287257e9cee8cb9dafc2
IsaiahZoop <iz19185p@pace.edu>
2025-07-03 18:19:39 -0400

    Fix Vite base path with trailing slash

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShopping-forked",
+  base: "/e-plantShopping-forked/",
   plugins: [react()],
 })
